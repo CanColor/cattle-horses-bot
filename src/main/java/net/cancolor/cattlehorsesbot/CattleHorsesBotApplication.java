f8264a13bbfed20c047a6647bd8b911b741d9995
xiangxiang <575604615>
2021-12-14 00:28:49 +0800

    1.封装登录

--- src/main/java/net/cancolor/cattlehorsesbot/CattleHorsesBotApplication.java
+++ src/main/java/net/cancolor/cattlehorsesbot/CattleHorsesBotApplication.java
@@ -1,9 +1,10 @@
 package net.cancolor.cattlehorsesbot;
 
 import io.netty.channel.Channel;
+import net.cancolor.easymiraiapi.channel.WebSocketMessageChannel;
+import net.cancolor.easymiraiapi.constent.ChannelConstant;
+import net.cancolor.easymiraiapi.factory.MessageChannelFactory;
 import net.cancolor.easymiraiapi.init.ImClientInit;
-import net.cancolor.easymiraiapi.model.message.dto.SendServerMessageDTO;
-import net.cancolor.easymiraiapi.utils.SendServerMessageUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -12,46 +13,44 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import javax.annotation.Resource;
 
-@SpringBootApplication(scanBasePackages="net.cancolor")
+@SpringBootApplication(scanBasePackages = "net.cancolor")
 public class CattleHorsesBotApplication implements CommandLineRunner {
     org.slf4j.Logger logger = LoggerFactory.getLogger(this.toString());
     @Resource
     ImClientInit imClientInit;
-    Channel channel ;
+
+    static Long botId = 2052737713L;
 
     public static void main(String[] args) {
         SpringApplication.run(CattleHorsesBotApplication.class, args);
     }
 
 
-
-
-
     @Override
     public void run(String... args) {
-        Thread webSocketThread=new Thread(new clientThread());
+        Thread webSocketThread = new Thread(new clientThread());
         webSocketThread.setName("websocket");
         webSocketThread.start();
     }
 
     public class clientThread implements Runnable {
-        Logger logger = LoggerFactory.getLogger(this.toString());
+        private Logger logger = LoggerFactory.getLogger(this.toString());
+        private Channel channel;
 
         @Override
         public void run() {
             imClientInit.connection();
-            login();
+            this.channel = imClientInit.getchannel();
+            try {
+                WebSocketMessageChannel messageChannel = (WebSocketMessageChannel) MessageChannelFactory.getMessageChannel(ChannelConstant.WEB_SOCKET);
+                //私聊
+                messageChannel.builder(channel, botId, 166748580L,575604615L).addAt().send();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
 
-
-        public void login(){
-            channel=imClientInit.getchannel();
-            SendServerMessageDTO sendServerMessageDTO =new SendServerMessageDTO();
-            sendServerMessageDTO.setComond("login");
-            sendServerMessageDTO.setClientId(1);
-            sendServerMessageDTO.setClientName("sb");
-
-        }
 
     }
 }
