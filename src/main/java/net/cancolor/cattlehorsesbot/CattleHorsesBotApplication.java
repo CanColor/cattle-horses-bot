a4c4cf16aea1780d49f77b5e68fe631b8180f4b1
xiangxiang <575604615@qq.com>
2021-12-15 14:42:24 +0800

    1.支持音乐分享

--- src/main/java/net/cancolor/cattlehorsesbot/CattleHorsesBotApplication.java
+++ src/main/java/net/cancolor/cattlehorsesbot/CattleHorsesBotApplication.java
@@ -5,6 +5,8 @@ import net.cancolor.easymiraiapi.channel.WebSocketMessageChannel;
 import net.cancolor.easymiraiapi.constent.ChannelConstant;
 import net.cancolor.easymiraiapi.factory.MessageChannelFactory;
 import net.cancolor.easymiraiapi.init.ImClientInit;
+import net.cancolor.easymiraiapi.model.message.MusicShareMessage;
+import net.mamoe.mirai.message.data.MusicKind;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -62,7 +64,18 @@ public class CattleHorsesBotApplication implements CommandLineRunner {
 //                messageChannel.builder(channel,botId,575604615L).addVipFace(VipFace.LiuLian).send();
 //                messageChannel.builder(channel, botId, 575604615L).addFlashImageByImageId("{94403FB8-2A62-3E24-521B-59A5F1A6C034}").send();
 //                messageChannel.builder(channel, botId, 575604615L).addFlashImageByUrl("https://ae01.alicdn.com/kf/H3333c75419484757805d349999842ff2m/XIAOMI-MIJIA-Portable-Car-Hand-Helded-Vaccum-Cleaner-for-home-Wireless-Mini-Dust-Catcher-Collector-13000Pa.jpg").send();
-                messageChannel.builder(channel, botId, 575604615L).addFlashImageByImageByFilePath("C:\\Users\\Administrator\\Desktop\\024f78f0f736afc3a3090d29a419ebc4b7451270.jpg").send();
+//                messageChannel.builder(channel, botId, 575604615L).addFlashImageByImageByFilePath("C:\\Users\\Administrator\\Desktop\\024f78f0f736afc3a3090d29a419ebc4b7451270.jpg").send();
+                MusicShareMessage musicShare = new MusicShareMessage(
+                        MusicKind.QQMusic,//播放器
+                        "哪吒",//歌名
+                        "蔡翊昇",//歌手
+                        //歌主页
+                        "https\\\\://i.y.qq.com/v8/playsong.html?songid=203838080&source=qq&ADTAG=pcsharetoqq",
+                        "http\\\\://y.gtimg.cn/music/photo_new/T002R300x300M000004PDvDb4ZxKrR.jpg",//图片
+                        //声源
+                        "http\\\\://c.y.qq.com/rsc/fcgi-bin/fcg_pyq_play.fcg?songmid=0024gHOb4GYnBO&songtype=0&fromtag=7&code=8d08d"
+                );
+                messageChannel.builder(channel, botId, 166748580L, 575604615L).addMusicShare(musicShare).send();
             } catch (Exception e) {
                 e.printStackTrace();
             }
