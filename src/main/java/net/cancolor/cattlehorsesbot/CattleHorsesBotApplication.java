1d9c01215ab0b1e4890a32f60c9ad9470aac8a37
xiangxiang <575604615@qq.com>
2021-12-15 10:48:56 +0800

    1.完成图片 闪照

--- src/main/java/net/cancolor/cattlehorsesbot/CattleHorsesBotApplication.java
+++ src/main/java/net/cancolor/cattlehorsesbot/CattleHorsesBotApplication.java
@@ -3,7 +3,6 @@ package net.cancolor.cattlehorsesbot;
 import io.netty.channel.Channel;
 import net.cancolor.easymiraiapi.channel.WebSocketMessageChannel;
 import net.cancolor.easymiraiapi.constent.ChannelConstant;
-import net.cancolor.easymiraiapi.constent.PokeConstant;
 import net.cancolor.easymiraiapi.factory.MessageChannelFactory;
 import net.cancolor.easymiraiapi.init.ImClientInit;
 import org.slf4j.Logger;
@@ -48,7 +47,7 @@ public class CattleHorsesBotApplication implements CommandLineRunner {
 //                messageChannel.builder(channel, botId, 166748580L,575604615L).addAt().addPlainText("1")
 //                        .addFace(1).addFace(2).send();
 
-                messageChannel.builder(channel, botId, 575604615L).addPokeMessage(PokeConstant.BI_XIN).send();
+//                messageChannel.builder(channel, botId, 575604615L).addPokeMessage(PokeConstant.BI_XIN).send();
                 //戳一戳
 //               messageChannel.builder(channel, botId, 166748580L,575604615L).nudge();
 //                messageChannel.builder(channel, botId, 575604615L).nudge();
@@ -61,7 +60,9 @@ public class CattleHorsesBotApplication implements CommandLineRunner {
 //                messageChannel.builder(channel, botId, 166748580L, 3021611449L).kick("给爷爪巴", false);
 
 //                messageChannel.builder(channel,botId,575604615L).addVipFace(VipFace.LiuLian).send();
-
+//                messageChannel.builder(channel, botId, 575604615L).addFlashImageByImageId("{94403FB8-2A62-3E24-521B-59A5F1A6C034}").send();
+//                messageChannel.builder(channel, botId, 575604615L).addFlashImageByUrl("https://ae01.alicdn.com/kf/H3333c75419484757805d349999842ff2m/XIAOMI-MIJIA-Portable-Car-Hand-Helded-Vaccum-Cleaner-for-home-Wireless-Mini-Dust-Catcher-Collector-13000Pa.jpg").send();
+                messageChannel.builder(channel, botId, 575604615L).addFlashImageByImageByFilePath("C:\\Users\\Administrator\\Desktop\\024f78f0f736afc3a3090d29a419ebc4b7451270.jpg").send();
             } catch (Exception e) {
                 e.printStackTrace();
             }
